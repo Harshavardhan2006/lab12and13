f5568f6da83946ccb1efb6f8ca3ea28150ac2162
Kokkonda Harshavardhan <kokkondaharshavardhan@gmail.com>
2026-03-13 15:51:57 +0530

    Initial lab12and13

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const {add} = require('./utils');
+
+const app = express();
+
+app.get('/sum', (req, res) => {
+    const result = add(5, 10);
+    res.json({result});
+});
+
+if (require.main === module) {
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+}
+
+module.exports = app;
